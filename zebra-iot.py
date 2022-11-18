8ad52d29a94695ae0552cf01ed005444a92a2eb8
Felipe Ignacio Domínguez Vega <felipe.dominguez@mercadolibre.cl>
2022-11-18 14:42:40 -0300

    Create zebra-iot.py

---
+++ zebra-iot.py
@@ -0,0 +1,21 @@
+import socket
+import os
+from _thread import *
+
+salir=False
+
+while not salir:
+    mysocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    print("Envio de configuracion Shipping-iot")
+    with open('shipping-iot.zpl', 'r') as f2:
+        data = f2.read()
+        print(data)
+    printer=str(input("Ingrese IP de impresora:"))
+    if printer == 'end':
+        salir = True
+    else:
+        port = 9100
+        mysocket.connect((printer,port))
+        mysocket.send(str.encode(data))
+        print(mysocket.recv(1024)) #recibe el ACK
+        mysocket.close()
