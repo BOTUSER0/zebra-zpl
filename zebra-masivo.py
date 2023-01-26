c3fa2755815d107ba2fffd005552e9c17d5e5921
Felipe Ignacio Domínguez Vega <felipe.dominguez@mercadolibre.cl>
2023-01-26 11:50:23 -0300

    Update zebra-masivo.py

--- zebra-masivo.py
+++ zebra-masivo.py
@@ -1,34 +1,29 @@
 import socket
 import os
 from _thread import *
-
-print("Envio de configuracion shipping-iot.zpl")
-
-def conn (printer):
-    mysocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    with open('shipping-iot.zpl', 'r') as f2:
-        data = f2.read()
-    port = 9100
-    mysocket.connect((printer,port))
-    while True:
-        mysocket.send(str.encode(data))
-        print(mysocket.recv(1024)) #recibe el ACK
-        mysocket.close()
-
-def printers ():
-    file = open('ipzebra.txt', 'r')
-    Lines = file.readlines()
-    count = 0
-
-    while True:  
-        for line in Lines:
-            count += 1
-            if not line:
-                break
-        print(line.strip())
-        printer = line.strip()
-        
-        return printer
+import re
 
 
-conn(printers())
+f1 = open('shipping-iot-psk.zpl', 'r')
+data = f1.read()
+
+
+f = open('ipzebra.txt', 'r') #Text file with many ip address
+o = f.read()
+ip1 = re.findall( r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", o )
+hosts = ip1
+ports = [9100]
+for host in hosts:
+    for port in ports:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+            result = s.connect_ex((host, port))
+            print(result)
+            if result == 0:
+                    s.send(str.encode(data))
+                    print (s.recv(1024)) #ACK
+                    print("  [*] Port " + str(port) + " open!" + host)
+            else: print("[+] CLOSE HOST " + host + ":" + str(port))
+        except:
+            pass
